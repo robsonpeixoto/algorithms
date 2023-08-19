43c66cec9838cb39b8c79a9ea5ee6a4aabf8f28d
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2023-08-19 12:16:34 -0300

    refactor: using builtin Go's cmp.Compare

--- comparable/comparable.go
+++ comparable/comparable.go
@@ -1,18 +1,8 @@
 package comparable
 
-import "golang.org/x/exp/constraints"
-
-// CompareTo is a function that compare two elements of the same type. Must
-// return a negativa, zero or positive number if the `lhs`(left hand side) is
-// smaller, equals of bigger than the `rhs`(right hand size)
-type CompareTo[T any] func(lhs, rhs T) int
-
-func CompareOrdered[T constraints.Ordered](lhs, rhs T) int {
-	ret := 0
-	if lhs > rhs {
-		ret = 1
-	} else if lhs < rhs {
-		ret = -1
-	}
-	return ret
-}
+// Compare returns
+//
+//	-1 if x is less than y,
+//	 0 if x equals y,
+//	+1 if x is greater than y.
+type Compare[T any] func(x, y T) int
--- grokking-algorithms/chap01/binary_search.go
+++ grokking-algorithms/chap01/binary_search.go
@@ -1,32 +1,32 @@
 package chap01
 
 import (
+	"cmp"
 	"errors"
 
 	"github.com/robsonpeixoto/algorithms/comparable"
-	"golang.org/x/exp/constraints"
 )
 
 var ErrNotFound = errors.New("item not found")
 
 // BinarySearch find a item in a sorted list. Returns (0, ErrNotFound) if item
 // does not exists.
-func BinarySearch[T constraints.Ordered](list []T, item T) (int, error) {
-	return BinarySearchBy(list, item, comparable.CompareOrdered[T])
+func BinarySearch[T cmp.Ordered](list []T, item T) (int, error) {
+	return BinarySearchBy(list, item, cmp.Compare)
 }
 
-func BinarySearchBy[T any](list []T, item T, compare comparable.CompareTo[T]) (int, error) {
+func BinarySearchBy[T any](list []T, item T, compare comparable.Compare[T]) (int, error) {
 	lowerBound := 0
 	upperBound := len(list) - 1
 	for lowerBound <= upperBound {
 		middleIdx := (lowerBound + upperBound) / 2
 		guess := list[middleIdx]
-		switch cmp := compare(guess, item); {
-		case cmp == 0:
+		switch res := compare(guess, item); res {
+		case 0:
 			return middleIdx, nil
-		case cmp > 0:
+		case 1:
 			upperBound = middleIdx - 1
-		case cmp < 0:
+		case -1:
 			lowerBound = middleIdx + 1
 		}
 	}
