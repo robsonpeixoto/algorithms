bc7622e28eecc48ce3b4b97a3eb8741417b36aa9
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2024-03-04 18:34:59 -0300

    refactor: remove Compare interface

--- comparable/comparable.go
+++
@@ -1,8 +0,0 @@
-package comparable
-
-// Compare returns
-//
-//	-1 if x is less than y,
-//	 0 if x equals y,
-//	+1 if x is greater than y.
-type Compare[T any] func(x, y T) int
--- grokking-algorithms/chap01/binary_search.go
+++ grokking-algorithms/chap01/binary_search.go
@@ -3,8 +3,6 @@ package chap01
 import (
 	"cmp"
 	"errors"
-
-	"github.com/robsonpeixoto/algorithms/comparable"
 )
 
 var ErrNotFound = errors.New("item not found")
@@ -12,16 +10,12 @@ var ErrNotFound = errors.New("item not found")
 // BinarySearch find a item in a sorted list. Returns (0, ErrNotFound) if item
 // does not exists.
 func BinarySearch[T cmp.Ordered](list []T, item T) (int, error) {
-	return BinarySearchBy(list, item, cmp.Compare)
-}
-
-func BinarySearchBy[T any](list []T, item T, compare comparable.Compare[T]) (int, error) {
 	lowerBound := 0
 	upperBound := len(list) - 1
 	for lowerBound <= upperBound {
 		middleIdx := (lowerBound + upperBound) / 2
 		guess := list[middleIdx]
-		switch res := compare(guess, item); res {
+		switch res := cmp.Compare(guess, item); res {
 		case 0:
 			return middleIdx, nil
 		case 1:
